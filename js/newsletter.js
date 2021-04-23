9fd485ad857c70f50638ead4a26a32aad214333c
unknown <joannasmerea@gmail.com>
2021-04-23 13:50:45 +0200

    code formatted

--- js/newsletter.js
+++ js/newsletter.js
@@ -1,19 +1,17 @@
-
 //NEWSLETTEER
 const getUserName = () => {
-  const user = prompt("Please enter your name");
-  if (user != null ) {
-    document.getElementById("user-name").innerHTML =
-    `Hola, ${user}.`;   
+  const user = prompt('Please enter your name');
+  if (user != null) {
+    document.getElementById('user-name').innerHTML = `Hola, ${user}.`;
   }
-}
+};
 
-window.onload=getUserName();
+window.onload = getUserName();
 
 const openForm = () => {
-  document.getElementById("myForm").style.display = "block";
-}
+  document.getElementById('myForm').style.display = 'block';
+};
 
-const closeForm = () =>{
-  document.getElementById("myForm").style.display = "none";
-}
+const closeForm = () => {
+  document.getElementById('myForm').style.display = 'none';
+};
--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // Get the container element
-const navbar = document.getElementById("navbar");
+const navbar = document.getElementById('navbar');
 // Get all elem with class="menu" inside the container
-const activeItem = navbar.getElementsByClassName("menu");
+const activeItem = navbar.getElementsByClassName('menu');
 
 // Loop through the elem and add the active class to the current/clicked button
 for (let i = 0; i < activeItem.length; i++) {
-  activeItem[i].addEventListener("click", function() {
-    const current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+  activeItem[i].addEventListener('click', function () {
+    const current = document.getElementsByClassName('active');
+    current[0].className = current[0].className.replace(' active', '');
+    this.className += ' active';
   });
 }
